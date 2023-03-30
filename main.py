484c9b59285253ddf4d5dd63f307a7454bc33413
Dylan Elliott <37644858+ElliottDylan60@users.noreply.github.com>
2023-03-29 18:16:50 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+    print("Hello World")
+
+"""
+Calculates and displays the frequency of each letter in the input string "cipherText"
+"""
+def letterCount(cipherText):
+    print("letter count")
+"""
+
+"""
+def plainText(cipherText, possibleKey):
+    print("Plain text")
+
+
+main()
