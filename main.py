f58d79ab7f9edda1d77d826e6395df506b547099
Dylan Elliott <37644858+ElliottDylan60@users.noreply.github.com>
2023-03-29 18:17:17 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+def main():
+    print("Hello World")
+
+"""
+Calculates and displays the frequency of each letter in the input string "cipherText"
+"""
+def letterCount(cipherText):
+    print("letter count")
+"""
+This function applies a substitution cipher to the input string "cipherText" 
+using the provided "key" to replace each character with a corresponding value.
+"""
+def plainText(cipherText, key):
+    print("Plain text")
+
+
+main()
