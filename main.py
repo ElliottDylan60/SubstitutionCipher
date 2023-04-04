22e9cee8012c3c5c3b0f1fa6e36b7f59b366f5ef
ElliottDylan60 <elliottdylan60@gmail.com>
2023-04-04 10:46:10 -0700

    Added main with error checking

--- main.py
+++ main.py
@@ -1,38 +1,37 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 30 11:28:33 2023
-
-@author: megan
-"""
-
 def main():
-    plainText("MXDXBVTZWVMXNSPBQXLIMSCCSGXSCJXBOVQXCJZMOJZCVCTVWJCZAAXZBCSSCJXBQCJZCOJZCNSPOXBXSBTVWJCJZDXGXXMOZQMSCSCJXBOVQXCJZMOJZCNSPJZHGXXMOSPLHJZDXZAAXZBXHCSCJXTCSGXSCJXBOVQX", "ZGEHXIWJVKFLTMSAYBQCPDORNU")
-    letterCount("MXDXBVTZWVMXNSPBQXLIMSCCSGXSCJXBOVQXCJZMOJZCVCTVWJCZAAXZBCSSCJXBQCJZCOJZCNSPOXBXSBTVWJCJZDXGXXMOZQMSCSCJXBOVQXCJZMOJZCNSPJZHGXXMOSPLHJZDXZAAXZBXHCSCJXTCSGXSCJXBOVQX")
+    cipherText = input("Please enter the cipher text: ")
+    letterCount(cipherText.upper())
+    key = input("Please enter a possible key: ")
+    if(len(key) > 26):
+        key = key[:26]
+    plainText(cipherText.upper(), key.upper())
+    
 """
 Calculates and displays the frequency of each letter in the input string "cipherText"
 """
-def letterCount(cipherText):
-    """
-    This function applies a substitution cipher to the input string "cipherText" 
-    using the provided "key" to replace each character with a corresponding value.
-    """  
+def letterCount(cipherText): 
     print("letter count: ")
     freq = {}
-    
     for i in cipherText:
         if i in freq:
             freq[i] += 1
         else:
             freq[i] = 1
-            
     print(freq)
-
-
+"""
+    This function applies a substitution cipher to the input string "cipherText" 
+    using the provided "key" to replace each character with a corresponding value.
+"""  
 def plainText(cipherText, key):
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     result = ""
     for letter in cipherText:
-        result += alphabet[key.find(letter)]
+        index = key.find(letter)
+        if(index > -1):
+            result += alphabet[index]
+        else:
+            result += letter
+    
     print(result)
 
 main()
